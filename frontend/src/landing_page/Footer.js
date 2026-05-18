a8bc91c259d95a5ca70d018a372d537a1942a69b
Tejas Sambhaji Jadhav <jtejas191@gmail.com>
2026-05-19 01:06:08 +0530

    homepage frontend work of all components done

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -2,7 +2,79 @@ import React from 'react';
 
 function Footer() {
     return ( 
-        <h1>Footer</h1>
+        <footer className='border-top' style={{backgroundColor:"rgb(250, 250, 250)"}}>
+            <div className='container'>
+                <div className='row mt-5'>
+                    <div className='col text-muted' style={{fontSize:"14px"}}>
+                        <img src='media/images/logo.svg' style={{width:"50%"}}/>
+                        <p>&copy; 2010-2024, Not Zerodha Broking Ltd.<br/>All rights reserved.</p>
+                        <a href='#' style={{color:"grey"}}><i class="fa-brands fa-twitter"></i></a>
+                        <a href='#' style={{padding:"0px 0px 0px 10px", color:"grey"}}><i class="fa-brands fa-facebook"></i></a>
+                        <a href='#' style={{padding:"0px 0px 0px 10px", color:"grey"}}><i class="fa-brands fa-instagram"></i></a>
+                        <a href='#' style={{padding:"0px 0px 0px 10px", color:"grey"}}><i class="fa-brands fa-linkedin"></i></a>
+                    </div>
+                    <div className='footer-links col'>
+                        <a href='#' style={{color:"black"}}>Company</a><br/>
+                        <a href='#'>About</a><br/>
+                        <a href='#'>Products</a><br/>
+                        <a href='#'>Pricing</a><br/>
+                        <a href='#'>Refferal programme</a><br/>
+                        <a href='#'>Carrers</a><br/>
+                        <a href='#'>Zerodha.tech</a><br/>
+                        <a href='#'>press & media</a><br/>
+                        <a href='#'>Zerodha cares (CSR)</a><br/>
+                    </div>
+                    <div className='col footer-links'>
+                        <a href='#' style={{color:"black"}}>Support</a><br/>
+                        <a href='#'>Contact</a><br/>
+                        <a href='#'>Support portal</a><br/>
+                        <a href='#'>Z-Connect blog</a><br/>
+                        <a href='#'>List of charges</a><br/>
+                        <a href='#'>Downloads & resources</a><br/>
+                    </div>
+                    <div className='col footer-links'>
+                        <a href='#' style={{color:"black"}}>Account</a><br/>
+                        <a href='#'>Open an account</a><br/>
+                        <a href='#'>Fund transfer</a><br/>
+                        <a href='#'>60 day challenge</a><br/>
+                    </div>
+                </div>
+                <div className='mt-5 text-muted' style={{fontSize:"14px"}}>
+                    <p>Zerodha Broking Ltd.: Member of NSE​ &​ BSE – SEBI Registration no.: INZ000031633 CDSL: Depository services through Zerodha Securities Pvt. Ltd. – SEBI Registration no.: IN-DP-100-2015 Commodity Trading through Zerodha Commodities Pvt. Ltd. MCX: 46025 – SEBI Registration no.: INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com, for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF</p>
+
+                    <p>
+                        Procedure to file a complaint on SEBI SCORES: Register on SCORES
+                        portal. Mandatory details for filing complaints on SCORES: Name,
+                        PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
+                        Communication, Speedy redressal of the grievances
+                    </p>
+
+                    <p>
+                        Investments in securities market are subject to market risks; read
+                        all the related documents carefully before investing.
+                    </p>
+
+                    <p>
+                        "Prevent unauthorised transactions in your account. Update your
+                        mobile numbers/email IDs with your stock brokers. Receive
+                        information of your transactions directly from Exchange on your
+                        mobile/email at the end of the day. Issued in the interest of
+                        investors. KYC is one time exercise while dealing in securities
+                        markets - once KYC is done through a SEBI registered intermediary
+                        (broker, DP, Mutual Fund etc.), you need not undergo the same
+                        process again when you approach another intermediary." Dear
+                        Investor, if you are subscribing to an IPO, there is no need to
+                        issue a cheque. Please write the Bank account number and sign the
+                        IPO application form to authorize your bank to make payment in case
+                        of allotment. In case of non allotment the funds will remain in your
+                        bank account. As a business we don't give stock tips, and have not
+                        authorized anyone to trade on behalf of others. If you find anyone
+                        claiming to be part of Zerodha and offering such services, please
+                        create a ticket here.
+                    </p>
+                </div>
+            </div>
+        </footer>
      );
 }
 
--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -2,8 +2,37 @@ import React from 'react';
 
 function Navbar() {
     return ( 
-        <h1>Navbar</h1>
-     );
+        <nav class="navbar navbar-expand-lg border-bottom" style={{backgroundColor:"#fff"}}>
+            <div class="container">
+                <a class="navbar-brand" href="#"> <img src='media/images/logo.svg' alt='logo' style={{width:"25%"}}/> </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    
+                </div>
+                <form class="d-flex" role="search">
+                    <ul class="navbar-nav mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Signup</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Support</a>
+                        </li>
+                    </ul>        
+                </form>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
